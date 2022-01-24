1b683ce0fd4b12bb1af1d9401a104e780e52932a
Adewale002 <97624760+Adewale002@users.noreply.github.com>
2022-01-23 19:37:10 -0800

    Task 2 completed

---
+++ Week1/Task2.py
@@ -0,0 +1,26 @@
+# Write a program that asks a user for their favourite number between 1 and 100 and then tells them a joke based on the number. You should use a minimum of 3 jokes.
+import random
+
+myName= input("Hello! What is your name?")
+
+favNum= random.randint(1,100)
+
+print("Well, "+ favNum+ ",I am thinking of a number between 1 and 100.")
+
+jokes=int("Hello! what is your favorite number?")
+
+# and then tells them a joke based on the number. You should use a minimum of 3 jokes.
+
+iffavNum ==50
+
+print(" Why is mosquto making lion cry?!" )
+
+eliffavNum >50
+
+print("three fish are in a tank ? one asks the others?''how do you drive this thing" )
+
+
+
+favNum< 50
+
+print("why chicken kills a cow? because the chicken is too powerful compare to cow.")
