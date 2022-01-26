ab9a2d1208742c56da6d665000417bf69b2a3d76
Adewale002 <97624760+Adewale002@users.noreply.github.com>
2022-01-26 06:24:58 -0800

    Task 5 Completed

---
+++ Week1/task5.py
@@ -0,0 +1,21 @@
+# Task 5
+# Write a program which will ask for two numbers from a user.
+
+questionA = input("Enter first number:")
+questionB = input("Enter second number:")
+
+# questionC = input("Enter first number:")
+# questionD = input("Enter second number:")
+
+# questionE = input("Enter first number:")
+# questionF = input("Enter second number:")
+
+
+sum = int(questionA)+int(questionB)
+# sum = int(questionC)*int(questionD)
+# sum = int(questionE)/int(questionF)
+
+print('sum 0f {0} and {1} is {2} '. format(questionA, questionB, sum))
+# print('sum 0f {0} and {1} is {2} '. format(questionC, questionD, sum))
+# print('sum 0f {0} and {1} is {2} '. format(questionE, questionF, sum))
+# Then offer a menu to the user giving them a choice of operator: e.g. –Enter “a” if you want to add“b” if you want to subtractInclude +, -, /, *, ** square (to the power of). Once the user has selected which operator they wish to use, perform the calculation.
