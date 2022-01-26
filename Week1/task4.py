a71c59b917c29750556c22e6cd1557703178e705
Adewale002 <97624760+Adewale002@users.noreply.github.com>
2022-01-26 05:49:56 -0800

    Task 4 Completed

---
+++ Week1/task4.py
@@ -0,0 +1,6 @@
+# A motorbike costs £2000 and loses 10% of its value every year. using a loop , print the value of the bike every following year it falss below £100
+
+cost = 2000
+while cost > 200:
+    print('the bike value is: ' ,cost)
+    cost -=200   # This is the same as count = count + 1
